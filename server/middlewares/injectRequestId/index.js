f947ba43ad7b8a60c03651e2b96fde200034579d
Praveen Kumar <praveenkumar@Praveens-MacBook-Pro.local>
2022-04-25 18:48:36 +0530

    better naming

--- server/middlewares/injectRequestId/index.js
+++ server/middlewares/injectRequestId/index.js
@@ -1,14 +1,14 @@
 import { v4 as uuid } from 'uuid';
 import log from 'utils/logger';
 
-function InjectRequestId(options, requestId) {
+function RequestIdInjectedLogger(options, requestId) {
     this.log = options.log.child({ requestId });
 }
 
 const injectRequestId = () => (_request, _response, next) => {
     const requestId = uuid();
-    const requestLogger = new InjectRequestId({ log }, requestId);
-    global.log = requestLogger.log;
+    const logger = new RequestIdInjectedLogger({ log }, requestId);
+    global.log = logger.log;
     next();
 };
 
