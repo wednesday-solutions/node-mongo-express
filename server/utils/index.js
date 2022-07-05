93aa3bcb8529c66ad294f2a318b9793267903939
Rushabh Sancheti <rushabhsanjaysancheti@Rushabhs-MacBook-Pro.local>
2022-07-05 16:19:24 +0530

    feat: adding validators for auto generated apis

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import mongoose from 'mongoose';
+import { apiFailure } from './apiUtils';
 
 export const isTestEnv = () =>
     process.env.ENVIRONMENT_NAME === 'test' || process.env.NODE_ENV === 'test';
@@ -10,4 +12,45 @@ export const getModelFiles = modelsFolderPath => {
     return fs
         .readdirSync(modelsFolderPath)
         .filter(file => fs.lstatSync(modelsFolderPath + file).isFile());
+};
+
+export const validateObjectId = (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params._id)) {
+        return apiFailure(res, { message: 'Invalid ObjectId' });
+    }
+    next();
+};
+
+export const validateSchema = model => (req, res, next) => {
+    const doc = new model(req.body);
+    doc.validate(function (err) {
+        if (err) {
+            return apiFailure(res, err.errors);
+        }
+        next();
+    });
+};
+
+export const validateReqBody = model => (req, res, next) => {
+    const validKeys = Object.keys(model.schema.obj);
+    const keys = Object.keys(req.body);
+    if (keys.length) {
+        let isValid = true;
+        for (let i = 0; i < keys.length; i++) {
+            if (!validKeys.includes(keys[i])) {
+                isValid = false;
+                break;
+            }
+        }
+        if (!isValid) {
+            return apiFailure(res, {
+                message: 'Request schema is invalid'
+            });
+        }
+        next();
+    } else {
+        return apiFailure(res, {
+            message: 'Request schema is invalid'
+        });
+    }
 };
